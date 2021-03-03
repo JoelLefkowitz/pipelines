25a15a2ad18fa6d166a67776053e4077b9315d7d
Joel <joellefkowitz@hotmail.com>
2021-03-03 00:27:10 +0000

    Update readme and configuration files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,18 @@
-fs = require("fs");
-YAML = require("yaml");
-
-function joinListEntries(x) {
-    return [x[0], x[1].join(" ")];
-}
-
-function joinObjAttrs(x) {
-    return Object.fromEntries(
-        Object.entries(x).map(joinListEntries)
-    );
-}
-
-function parseYaml(x) {
-    return YAML.parse(fs.readFileSync(x, "utf8"));
+function quit(name) {
+     console.log(
+         "Task: \'".concat(name, "\' has not been implemented")
+    )
 }
 
 module.exports = function (grunt) {
-    const execs = joinObjAttrs(parseYaml("routines.yml"));
-    const force = ["prettier"];
-
-    grunt.initConfig({ exec: execs });
-    grunt.loadNpmTasks("grunt-exec");
-    grunt.loadNpmTasks("grunt-force-task");
-
-    for (let name of Object.keys(execs)) {
-        force.includes(name)
-            ? grunt.registerTask(name, "force:exec:" + name)
-            : grunt.registerTask(name, "exec:" + name);
-    }
-
-    grunt.registerTask("lint", ["cspell", "pylint", "bandit"]);
-
-    grunt.registerTask("format", [
-        "presort",
-        "black",
-        "autoflake",
-        "isort",
-        "prettier",
-    ]);
-
-    grunt.registerTask("unitTests", ["pytest"]);
-
-    grunt.registerTask("prebuild", [
-        "format",
-        "lint",
-        "unitTests",
-    ]);
-
-    grunt.registerTask("dev", ["buildDocker", "runDev"]);
-};
+    grunt.initConfig();
+    grunt.registerTask("run", env => quit("run"));
+    grunt.registerTask("build", env => quit("build"));
+    grunt.registerTask("tests", target => quit("tests"));
+    grunt.registerTask("docs", target => quit("docs"));
+    grunt.registerTask("lint", () => quit("lint"));
+    grunt.registerTask("format", () => quit("format"));
+    grunt.registerTask("precommit", [
+        "lint", "format", "tests:coverage", "docs:generate"
+    ])
+}
